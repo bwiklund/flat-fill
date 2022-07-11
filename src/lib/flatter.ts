c77f92b01820ff6ccb3bc5049621bc67b862467e
Ben Wiklund <ben@daisyowl.com>
2022-07-10 21:13:13 -0700

    whoa

--- src/lib/flatter.ts
+++ src/lib/flatter.ts
@@ -1,6 +1,7 @@
 import { Vec } from "./vec";
 //@ts-ignore
 import calcSdf from "bitmap-sdf";
+import { orderBy } from "lodash";
 
 interface Pigment {
   pos: Vec;
@@ -29,8 +30,8 @@ export function flatter(img: HTMLImageElement) {
 
   //show distances
   let imgArr = new Uint8ClampedArray(w * h * 4);
-  for (let i = 0; i < w; i++) {
-    for (let j = 0; j < h; j++) {
+  for (let j = 0; j < h; j++) {
+    for (let i = 0; i < w; i++) {
       imgArr[j * w * 4 + i * 4 + 0] = distances[j * w + i] * 255;
       imgArr[j * w * 4 + i * 4 + 1] = distances[j * w + i] * 255;
       imgArr[j * w * 4 + i * 4 + 2] = distances[j * w + i] * 255;
@@ -41,24 +42,70 @@ export function flatter(img: HTMLImageElement) {
 
   // do the work, grow the pigments and merge ones that overlap without hitting lines first
 
-  for (var i = 0; i < w * h; i++) {
-    // TODO whats a clever cheap way to sample this "randomly" enough but without using memory to do it
-    var idx = i;
-    var I = 4 * idx;
-    var x = idx % w;
-    var y = Math.floor(idx / w);
-    imdata.data[I + 0] = Math.random() * 255;
-    imdata.data[I + 1] = Math.random() * 255;
-    imdata.data[I + 2] = Math.random() * 255;
-    imdata.data[I + 3] = 255;
+  // start with the furthest pixels from lines and start filling
+  // format is idx, distance
+  // TODO this is probably slow and could have a more clever data layout on a native array
+  var idxByDistance: [number, number][] = [];
+  for (let j = 0; j < h; j++) {
+    for (let i = 0; i < w; i++) {
+      const idx = j * w + i;
+      idxByDistance.push([idx, distances[idx]]);
+    }
   }
 
-  // ctx.putImageData(imdata, 0, 0);
-  ctx.putImageData(sdfDebugImage, 0, 0);
+  var maxIdx = Math.pow(2, 32);
+
+  var idxInOrder = orderBy(idxByDistance, (tup) => -tup[1]).map(
+    (tup) => tup[0],
+  );
+
+  let idxPainting = new Uint32Array(w * h);
+  for (var i = 0; i < idxInOrder.length; i++) {
+    // TODO whats a clever cheap way to sample this "randomly" enough but without using memory to do it
+    var idx = idxInOrder[i];
+
+    // is reserved for unvisited idx's
+    var x = idx % w;
+    var y = Math.floor(idx / w);
+    var anyNeighborColor = 0;
+    var found = false;
+    for (var ox = -1; ox <= 1; ox++) {
+      for (var oy = -1; oy <= 1; oy++) {
+        var xx = x + ox;
+        var yy = y + oy;
+        if (xx < 0 || xx >= w || yy < 0 || yy >= h) continue;
+
+        var neighborPainted = idxPainting[yy * w + xx];
+        if (neighborPainted !== 0) {
+          anyNeighborColor = neighborPainted;
+          found = true;
+          break; // or do this in a randomized order?
+        }
+      }
+
+      if (found) break;
+    }
+
+    idxPainting[idx] = found ? anyNeighborColor : Math.random() * maxIdx;
+  }
+
+  for (let y = 0; y < h; y++) {
+    for (let x = 0; x < w; x++) {
+      var idx = y * w + x;
+      var I = 4 * idx;
+      imdata.data[I + 0] = (idxPainting[y * w + x] * 255) / maxIdx;
+      imdata.data[I + 1] = (idxPainting[y * w + x] * 255) / maxIdx;
+      imdata.data[I + 2] = (idxPainting[y * w + x] * 255) / maxIdx;
+      imdata.data[I + 3] = 255;
+    }
+  }
+
+  ctx.putImageData(imdata, 0, 0);
+  // ctx.putImageData(sdfDebugImage, 0, 0);
 
   // draw the flatted version. actually return both canvases so the user can toggle it
   ctx.globalCompositeOperation = "multiply";
-  // ctx.drawImage(img, 0, 0);
+  ctx.drawImage(img, 0, 0);
 
   return canvas;
 }
